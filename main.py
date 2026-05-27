abd39d962000b824c3fed9655ce46ec96772f2d1
LeiaSilva5 <leiaferreira457@gmail.com>
2026-05-26 21:43:25 -0300

    funcao cadastro_aluno

---
+++ main.py
@@ -0,0 +1,28 @@
+# Projeto Nota Escolar
+# Funoes do sistema f
+def cadastro_aluno(quant,lista):
+
+    i = 0
+    for i in range(quant):
+        nome = input('Digite o nome do aluno: ').upper()
+        if nome.isalpha():
+            nota1 = float(input('Digite a primeira nota: '))
+            nota2 = float(input('Digite a segunda nota: '))
+            nota3 = float(input('Digite a terceira nota: '))
+            aluno = {
+                'Nome:' : nome,
+                'Nota_1' : nota1,
+                'Nota_2' : nota2,
+                'Nota_3' : nota3
+            }
+            lista.append(aluno)
+        else:
+            print('Digite um nome valido')
+            continue
+    return lista
+
+# Código principal
+lista_alunos = []
+print('Sistema de Notas Escolares')
+quantidade = int(input('Digite a quantidade de alunos: '))
+print(cadastro_aluno(quantidade,lista_alunos))
---
+++ teste.py
@@ -0,0 +1,24 @@
+# nome = input('Digite o seu nome: ')
+# # numero = float(input('Digite um número'))
+# # Verificação do nome
+# print(nome.isnumeric())
+# print(nome.isdigit())
+# print(nome.isalpha())
+# print(nome.isdecimal())
+
+
+# numero_float = 9.3
+# teste = type(numero_float)
+
+# if teste == teste:
+#     print('numeor é float')
+# else:
+#     print('não é')
+
+
+numero = float(input('Digite um número : '))
+
+if 0 < numero <=  10 :
+    print('Numero está entre zero e 10')
+else:
+    print('Esse numero está fora do range')
